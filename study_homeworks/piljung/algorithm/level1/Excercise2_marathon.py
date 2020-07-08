214e10458334a70a2d4f32294b7fd4ca7dc80354
김필정 <62067617+pjjoy@users.noreply.github.com>
2020-07-09 01:01:48 +0900

    commit(folder name changed)

--- study_homeworks/piljung/algorithm/level1/Excercise2_marathon.py
+++ study_homeworks/piljung/algorithm/level1/Excercise2_marathon.py
@@ -1,5 +1,5 @@
 def solution(participant, completion):
     for man in completion:
-        participant.remove(man)
-    answer = participant[0]
+        participant.remove(man) # 완주자를 참가자에서 제외하기
+        answer = participant[0] # 항상 1명만 완주하지 못한것이므로 다 제외하고 남은 하나의 원소(?) 추출
 return answer
