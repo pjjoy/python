b9e939727ec1c25e57f9e4146c4c945d9c78431e
김필정 <62067617+pjjoy@users.noreply.github.com>
2020-07-09 00:58:10 +0900

    commit(folder name changed)

--- study_homeworks/piljung/algorithm/level1/Excercise1_pickdoll.py
+++ study_homeworks/piljung/algorithm/level1/Excercise1_pickdoll.py
@@ -4,10 +4,11 @@ def solution(board, moves):
     kkofriend = 0
 
     for move in moves:
-        for i in range(len(board)):
+        for i in range(len(board)): # 열 수 기준
             if board[i][move - 1] > 0:
-                kkofriend = board[i][move - 1]
-                board[i][move - 1] = 0
+                kkofriend = board[i][move - 1] # 집은 인형 세팅
+                board[i][move - 1] = 0 # 인형있던 자리 비우기
+
                 if len(basket) > 0 and basket[-1] == kkofriend:  # 마지막=새로운거
                     answer += 2  # 사라진 결과값 더하기
                     basket.pop()  # 리스트에서 마지막 항목 삭제
