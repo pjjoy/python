038609aa60467408da0bbc76838722a040a41a54
김필정 <62067617+pjjoy@users.noreply.github.com>
2020-07-13 02:00:49 +0900

    commit

--- study_homeworks/piljung/algorithm/level1/Excercise4_gymsuit.py
+++ study_homeworks/piljung/algorithm/level1/Excercise4_gymsuit.py
@@ -1,18 +1,35 @@
+#일부 테스트 통과 못한 로직
 def solution(n, lost, reserve):
     save = []
+    resetlost = set(lost) - set(reserve)
+    resetreserve = set(reserve) - set(lost)  # 여분있는 사람이 도난 당했을 경우 빌려줄 수 없으므로 삭제
 
-    if len(reserve) < len(lost):
-        for lostst in lost:
-            for reservest in reserve:
+    if len(resetreserve) <= len(resetlost):
+        for lostst in resetlost:
+            for reservest in resetreserve:
                 if lostst == reservest - 1 or lostst == reservest + 1:
                     save.append(reservest)
     else:
-        for lostst in lost:
-            for reservest in reserve:
+        for lostst in resetlost:
+            for reservest in resetreserve:
                 if reservest == lostst - 1 or reservest == lostst + 1:
                     save.append(lostst)
 
-    a = len(list(set(save)))  # 중복 제거
+    a = len(list(set(save)))  #
     answer = n - len(lost) + a
 
     return answer
+
+
+#최종 풀이
+
+def solution(n, lost, reserve):
+    resetlost = set(lost) - set(reserve)
+    resetreserve = set(reserve) - set(lost)  # 여분있는 사람이 도난 당했을 경우 빌려줄 수 없으므로 삭제
+    for i in resetreserve:
+        if i - 1 in resetlost:
+            resetlost.remove(i - 1)
+        elif i + 1 in resetlost:
+            resetlost.remove(i + 1)
+
+    return n - len(resetlost)
