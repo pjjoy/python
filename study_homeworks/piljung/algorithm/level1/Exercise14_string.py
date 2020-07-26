c9344fd90f6e56aacc77e10382355bec65793dc3
김필정 <62067617+pjjoy@users.noreply.github.com>
2020-07-27 01:17:20 +0900

    piljung commit

--- study_homeworks/piljung/algorithm/level1/Exercise14_string.py
+++ study_homeworks/piljung/algorithm/level1/Exercise14_string.py
@@ -1,12 +1,12 @@
 #14. 문자열 다루기 기본
 
+#런타임 애러나는 내 풀이
 def solution(s):
     if (len(s) == 4 or len(S) == 6) and s.isdigit():  # isdigit() : 문자열이 숫자인지 확인 /* 참고: 문자열이 문자가 아닌지 판별하는 함수 isalpha
-        answer = True
+        return True
     else:
-        answer = False
+        return False
 
-    return answer
 
 #다른 사람 풀이 (늘여 쓰지 않아도 되는 군)
 def solution(s):
