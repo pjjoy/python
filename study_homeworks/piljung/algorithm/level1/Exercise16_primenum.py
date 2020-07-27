8b16f8d72e70d9af95d6c0df39cae2489f2dbd23
KIM_PILJUNG01 <littlecat7409@gmail.com>
2020-07-27 16:12:52 +0900

    piljung commit

--- study_homeworks/piljung/algorithm/level1/Exercise16_primenum.py
+++ study_homeworks/piljung/algorithm/level1/Exercise16_primenum.py
@@ -25,3 +25,16 @@ def solution(n):
 9. 자기 자신을 제외한 7의 배수를 모두 지운다.
 위의 과정을 반복하면 구하는 구간의 모든 소수가 남는다.
 '''
+def prime_list(n):
+    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    sieve = [True] * n
+
+    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사 (루트)
+    m = int(n ** 0.5) # math라이브러리를 쓰지 않는 이상 ** 로 루트 계산
+    for i in range(2, m + 1):
+        if sieve[i] == True:           # i가 소수인 경우
+            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return [i for i in range(2, n) if sieve[i] == True]
