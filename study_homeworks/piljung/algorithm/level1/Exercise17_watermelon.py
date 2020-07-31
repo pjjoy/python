2ca8578431ced8646195f448bcc6185f86abb0e7
김필정 <62067617+pjjoy@users.noreply.github.com>
2020-07-31 11:17:34 +0900

    piljung commit

---
+++ study_homeworks/piljung/algorithm/level1/Exercise17_watermelon.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = ''
+
+    for i in range(0, n):
+        if i % 2 == 0:
+            answer += "수"  # 문자도 더할수 있음
+        else:
+            answer += "박"
+    return answer
