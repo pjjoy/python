59386ce295d6ceae2d2ad72cd2c83e2f5a4efbd0
KIM_PILJUNG01 <littlecat7409@gmail.com>
2020-08-31 16:29:23 +0900

    piljung commit

--- study_homeworks/piljung/algorithm/level1/Exercise24_descSortedNumber.py
+++ study_homeworks/piljung/algorithm/level1/Exercise24_descSortedNumber.py
@@ -1,4 +1,4 @@
 # 24. 정수 내림차순으로 배치하기
 
 def solution(n):
-    return int(''.join(sorted(str(n), reverse=True)))  #문자열로 리스트 정리하여 붙이기
+    return int(''.join(sorted(str(n), reverse=True)))  #문자열로 새로운 리스트 정렬하여 생성한 다음(* sorted : 신규리스트생성) join으로 정리하여 붙이기
