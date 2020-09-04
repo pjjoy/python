dd0f1e69ccbb0d0cebbb01fb5e2fb3b9fc822342
KIM_PILJUNG01 <littlecat7409@gmail.com>
2020-09-04 12:55:26 +0900

    piljung commit

--- study_homeworks/piljung/algorithm/level2/Exercise3_truckonbridge.py
+++ study_homeworks/piljung/algorithm/level2/Exercise3_truckonbridge.py
@@ -1,1 +1,15 @@
 # 3. 다리를 지나는 트럭
+
+def solution(bridge_length, weight, truck_weights):
+    bridge=[0]*bridge_length#[0 for x in range(bridge_lenght)]
+    time = 0
+    while bridge:
+        time += 1
+        bridge.pop(0)
+        if truck_weights:
+            if sum(bridge) + truck_weights[0] <= weight:
+                bridge.append(truck_weights.pop(0))
+            else:
+                bridge.append(0)
+
+    return time
