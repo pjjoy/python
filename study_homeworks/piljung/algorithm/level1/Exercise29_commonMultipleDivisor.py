9feaf7417a2821ed573f475f49279d604a888c8d
KIM_PILJUNG01 <littlecat7409@gmail.com>
2020-09-11 15:10:34 +0900

    piljung commit

--- study_homeworks/piljung/algorithm/level1/Exercise29_commonMultipleDivisor.py
+++ study_homeworks/piljung/algorithm/level1/Exercise29_commonMultipleDivisor.py
@@ -1,6 +1,5 @@
 # 29. 최대공약수와 최소 공배수
 
-
 # a,b  의 최대공약수는 (a%b),b 의 최대공약수와 같다.
 # 최소공배수는 a과 b의 곱에 최대공약수를 나누어 준 것과 같다.
 
@@ -10,8 +9,7 @@ def solution(n, m):
 
     mod = a % b
     while mod > 0:
-        a = b
-        b = mod
+        a, b = b, mod
         mod = a % b
 
     return [b, m * n // b]
