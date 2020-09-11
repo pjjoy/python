c27f0b108a3e1d3111ba2f286d077cef1c8a1368
김필정 <62067617+pjjoy@users.noreply.github.com>
2020-09-12 08:45:58 +0900

    piljung commit

---
+++ study_homeworks/piljung/algorithm/level1/Exercise29_commonMultipleDivisor.py
@@ -0,0 +1,15 @@
+# 29. 최대공약수와 최소공배수
+
+# a,b  의 최대공약수는 (a%b),b 의 최대공약수와 같다.
+# 최소공배수는 a과 b의 곱에 최대공약수를 나누어 준 것과 같다.
+
+def solution(n, m):
+    a = n
+    b = m
+
+    mod = a % b
+    while mod > 0:
+        a, b = b, mod
+        mod = a % b
+
+    return [b, m * n // b]
---
+++ study_homeworks/piljung/algorithm/level1/Exercise31_average.py
@@ -0,0 +1,5 @@
+# 31. 평균 구하기
+
+def solution(arr):
+
+    return sum(arr)/len(arr)
