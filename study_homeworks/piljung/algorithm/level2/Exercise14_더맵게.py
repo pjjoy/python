fe97d16c8a8255e3052a2084f565d2e1070f16e1
김필정 <62067617+pjjoy@users.noreply.github.com>
2021-02-04 22:13:44 +0900

    piljung commit

---
+++ study_homeworks/piljung/algorithm/level2/Exercise14_더맵게.py
@@ -0,0 +1,17 @@
+# 14.  맵게
+
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0]<K :
+        if len(scoville)>1:  # 1개 있을때 굳이 돌릴 필요 없음... break하고 마무리하는게 좋음
+            a=heapq.heappop(scoville)
+            b=heapq.heappop(scoville)
+            heapq.heappush(scoville, a+b*2)
+            answer+=1
+        else:
+            answer=-1
+            break
+    return answer
