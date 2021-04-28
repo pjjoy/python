66f9c212f129eb1979c8f03df401ea19dc388468
kim <khyunjae10@gmail.com>
2021-04-28 22:27:18 +0900

    level3-network

--- study_homeworks/hyunjae/algorithm/level3/Exercise01_network.py
+++ study_homeworks/hyunjae/algorithm/level3/Exercise01_network.py
@@ -4,27 +4,23 @@ def solution(n, computers):
     queue = deque()
     visit = [False for _ in range(n)]
 
-    for i in visit:
+    for idx_1, i in enumerate(visit):
         if not i:
+            if not queue:
+                queue.append(computers[idx_1])
+                visit[idx_1] = True
             while queue:
                 for idx, i in enumerate(queue[0]):
-                    if (not visit[idx]) and i == 1:
+                    if idx != idx_1 and (not visit[idx]) and i == 1:
                         queue.append(computers[idx])
                         visit[idx] = True
-        network += 1
-
-
-
-
-
-
-
-
-
-
-    pass
+                queue.popleft()
+            network += 1
+    return network
 
 
 n = 3
 computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+n = 4
+computers = [[1, 0, 1, 1], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
 print(solution(n, computers))
